addf02dd3da888aaba50ee92c7f76585bff2fa62
michele byman <michelebyman@gmail.com>
2019-09-04 12:12:22 +0200

    sticky info

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+         <div class="info-bar">
+                            <p class="info-library">
+                                Vue.js
+                                <i class="fas fa-wifi"></i>
+                            </p>
+                            <p class="info-library">{{ time }}</p>
+                            <p class="info-library">
+                                100%
+                                <i class="fas fa-battery-full"></i>
+                            </p>
+                        </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,18 +20,22 @@
 </template>
 
 <style>
+*html {
+        box-sizing: border-box;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+    background-color: #4d4e50;
 }
 #nav {
   padding: 30px;
-  position: sticky;
-  top: 0;
-  background-color: white;
+  /* position: sticky;
+  top: 0; */
+ background-color: #4d4e50;
 }
 
 #nav a {
@@ -30,5 +45,16 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.info-bar {
+        width: 100%;
+    color: white;
+    background-color: #4d4e50;
+    font-size: 0.7rem;
+    display: flex;
+    justify-content: space-between;
+    position: sticky;
+    top: 0px;
 }
 </style>
