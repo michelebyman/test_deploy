95da20b78845be93876b1abc1e03d70683ff5237
Michele Byman <michelebyman@gmail.com>
2020-11-03 10:19:47 +0100

    svenska användaravtal

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -1,143 +1,145 @@
 <template>
     <div class="policy-wrapper">
-        <h1>Privacy Policy for Sukha, the personal development application</h1>
+        <h1>
+            Användaravtal för Sukha, applikationen som får dig och dina vänner
+            att utvecklas.
+        </h1>
 
         <p>
-            At Sukha, accessible from https://michelebyman.netlify.app/sukha,
-            one of our main priorities is the privacy of our visitors. This
-            Privacy Policy document contains types of information that is
-            collected and recorded by Sukha and how we use it.
+            På Sukha, tillgänglig från https://michelebyman.netlify.app/sukha,
+            en av våra viktigaste prioriteringar är våra besökares integritet.
+            Detta Sekretesspolicy innehåller information som finns samlas in och
+            spelas in av Sukha och hur vi använder det.
         </p>
 
         <p>
-            If you have additional questions or require more information about
-            our Privacy Policy, do not hesitate to contact us
+            Om du har ytterligare frågor eller behöver mer information om vår
+            integritetspolicy, tveka inte att kontakta oss
             https://michelebyman.netlify.app/contact.
         </p>
 
         <p>
-            This Privacy Policy applies only to our online activities and is
-            valid for visitors to our application with regards to the
-            information that they shared and/or collect in Sukha.
+            Denna sekretesspolicy gäller endast våra onlineaktiviteter och är
+            giltigt för besökare till vår ansökan med avseende på information
+            som de delade och / eller samlar in i Sukha.
         </p>
 
-        <h2>Consent</h2>
+        <h2>Samtycke</h2>
 
         <p>
-            By using our application, you hereby consent to our Privacy Policy
-            and agree to its terms.
+            Genom att använda vår ansökan godkänner du härmed vår
+            integritetspolicy och godkänner dess villkor.
         </p>
 
-        <h2>Terms and condition and information we collect</h2>
+        <h2>Villkor och information vi samlar in</h2>
 
         <p>
-            The personal information that you are asked to provide, and the
-            reasons why you are asked to provide it, will be made clear to you
-            at the point we ask you to provide your personal information.
+            Den personliga informationen som du ombeds lämna och skäl till
+            varför du ombeds att tillhandahålla den kommer att göras tydlig för
+            dig när vi ber dig att tillhandahålla din personliga information.
         </p>
         <p>
-            If you contact us directly, we may receive additional information
-            about you such as your name, email address, phone number, the
-            contents of the message and/or attachments you may send us, and any
-            other information you may choose to provide.
+            Om du kontaktar oss direkt kan vi få ytterligare information om dig
+            som ditt namn, e-postadress, telefonnummer, innehållet i meddelandet
+            och / eller bilagor som du kan skicka till oss och eventuella annan
+            information du kan välja att lämna.
         </p>
         <p>
-            You do not need to register for an Account, we do not ask for your
-            contact information, such as name, company name, address, email
-            address, and telephone number. Therefore, we do not collect any data
-            from you by using by using our application.
+            Du behöver inte registrera dig för ett konto, vi ber inte om ditt
+            kontaktinformation, såsom namn, företagsnamn, adress, e-post adress
+            och telefonnummer. Därför samlar vi inte in några uppgifter från dig
+            genom att använda vår applikation.
         </p>
 
-        <h2>How we use your information</h2>
+        <h2>Hur vi använder din information</h2>
 
         <p>
-            We use the information we collect (emails or phone calls) in various
-            ways, including to:
+            Vi använder informationen vi samlar in (e-post eller telefonsamtal)
+            i olika sätt, inklusive att:
         </p>
 
         <ul>
-            <li>Provide, operate, and maintain our application</li>
-            <li>Improve, personalize, and expand our application</li>
-            <li>Develop new products, services, features, and functionality</li>
+            <li>Tillhandahålla, driva och underhålla vår applikation</li>
+            <li>Förbättra, anpassa och utöka vår applikation</li>
+            <li>Utveckla nya produkter, tjänster, funktioner och funktioner</li>
             <li>
-                Communicate with you by email or phone, either directly,
-                including for customer service, to provide you with updates and
-                other information relating to the application.
+                Kommunicera med dig via e-post eller telefon, antingen direkt,
+                inklusive för kundtjänst, för att ge dig uppdateringar och annan
+                information som rör ansökan.
             </li>
-            <li>Send you emails</li>
-            <li>Find and prevent fraud</li>
+            <li>Skicka e-postmeddelanden till dig</li>
+            <li>Hitta och förhindra bedrägerier</li>
         </ul>
 
-        <h2>Log Files</h2>
+        <h2>Loggfiler</h2>
 
         <p>
-            Sukha follows a standard procedure of using log files. These files
-            log visitors when they visit websites/applications. All hosting
-            companies do this and a part of hosting services' analytics. The
-            information collected by log files include internet protocol (IP)
-            addresses, browser type, Internet Service Provider (ISP), date and
-            time stamp, referring/exit pages, and possibly the number of clicks.
-            These are not linked to any information that is personally
-            identifiable. The purpose of the information is for analyzing
-            trends, administering the site, tracking users' movement on the
-            website, and gathering demographic information.
+            Sukha följer en standardprocedur för användning av loggfiler. Dessa
+            filer logga besökare när de besöker webbplatser / applikationer.
+            Alla värdar företag gör detta och en del av värdtjänstens analys. De
+            information som samlas in av loggfiler inkluderar internetprotokoll
+            (IP) adresser, webbläsartyp, Internetleverantör (ISP), datum och
+            tidsstämpel, hänvisnings- / utgångssidor och eventuellt antalet
+            klick. Dessa är inte kopplade till någon personlig information
+            identifierbar. Syftet med informationen är att analysera trender,
+            administrering av webbplatsen, spårning av användarnas rörelse på
+            webbplats och samla demografisk information.
         </p>
 
-        <h2>GDPR Data Protection Rights</h2>
+        <h2>GDPR Dataskyddsrättigheter</h2>
 
         <p>
-            We would like to make sure you are fully aware of all of your data
-            protection rights. Every user is entitled to the following:
+            Vi vill se till att du är helt medveten om all din information
+            skyddsrättigheter. Varje användare har rätt till följande:
         </p>
         <p>
-            The right to access – You have the right to request copies of your
-            personal data. We may charge you a small fee for this service.
+            Rätten till åtkomst - Du har rätt att begära kopior av din personlig
+            information. Vi kan ta ut en liten avgift för denna tjänst.
         </p>
         <p>
-            The right to rectification – You have the right to request that we
-            correct any information you believe is inaccurate. You also have the
-            right to request that we complete the information you believe is
-            incomplete.
+            Rätten till rättelse - Du har rätt att begära att vi korrigera all
+            information du tror är felaktig. Du har också rätt att begära att vi
+            fyller i den information du tror är Ofullständig.
         </p>
         <p>
-            The right to erasure – You have the right to request that we erase
-            your personal data, under certain conditions.
+            Rätten att radera - Du har rätt att begära att vi raderar dina
+            personuppgifter, under vissa förhållanden.
         </p>
         <p>
-            The right to restrict processing – You have the right to request
-            that we restrict the processing of your personal data, under certain
-            conditions.
+            Rätten att begränsa behandlingen - Du har rätt att begära att vi
+            under vissa omständigheter begränsar behandlingen av dina
+            personuppgifter betingelser.
         </p>
         <p>
-            The right to object to processing – You have the right to object to
-            our processing of your personal data, under certain conditions.
+            Rätten att invända mot bearbetning - Du har rätt att invända mot vår
+            behandling av dina personuppgifter, under vissa villkor.
         </p>
         <p>
-            The right to data portability – You have the right to request that
-            we transfer the data that we have collected to another organization,
-            or directly to you, under certain conditions.
+            Rätten till dataportabilitet - Du har rätt att begära det vi överför
+            de uppgifter som vi har samlat till en annan organisation, eller
+            direkt till dig, under vissa förhållanden.
         </p>
         <p>
-            If you make a request, we have one month to respond to you. If you
-            would like to exercise any of these rights, please contact us.
-        </p>
-
-        <h2>Children's Information</h2>
-
-        <p>
-            Another part of our priority is adding protection for children while
-            using the internet. We encourage parents and guardians to observe,
-            participate in, and/or monitor and guide their online activity.
+            Om du gör en begäran har vi en månad att svara på dig. Om du vill
+            utöva någon av dessa rättigheter, vänligen kontakta oss.
         </p>
 
+        <h2>Barninformation</h2>
+
         <p>
-            Sukha does not knowingly collect any Personal Identifiable
-            Information from children of any age. If you think that your child
-            provided this kind of information on our website, we strongly
-            encourage you to contact us immediately and we will do our best
-            efforts to promptly remove such information from our records, it
-            could only be by email, becuase our application does not collect any
-            data.
+            En annan del av vår prioritering är att lägga till skydd för barn
+            medan använda internet. Vi uppmuntrar föräldrar och vårdnadshavare
+            att observera, delta i och / eller övervaka och vägleda deras
+            online-aktivitet.
+        </p>
+
+        <p>
+            Sukha samlar inte medvetet personligt identifierbart Information
+            från barn i alla åldrar. Om du tror att ditt barn tillhandahållit
+            denna typ av information på vår webbplats, vi starkt uppmuntra dig
+            att kontakta oss omedelbart så gör vi vårt bästa ansträngningar för
+            att snabbt ta bort sådan information från våra register kan bara ske
+            via e-post, eftersom vår ansökan inte samlar in någon data.
         </p>
     </div>
 </template>
